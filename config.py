715aa10dc1d6f3fbe74b09837bd9e606f3d951d6
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-02-14 10:36:58 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,33 +5,6 @@ import random
 os.system('cls' if os.name == 'nt' else 'clear')
 
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(7, GPIO.)
-GPIO.setup(8, GPIO.OUT))
-GPIO.setup(9, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
-
-def parar():
-  GPIO.output(7,False)
-  GPIO.output(8,False)
-  GPIO.output(9,False)
-  GPIO.output(10,False)
-def frente():
-  GPIO.output(7,False)
-  GPIO.output(8,True)
-  GPIO.output(9,False)
-  GPIO.output(10,True)
-def direita():
-  GPIO.output(7,False)
-  GPIO.output(8,True)
-  GPIO.output(9,True)
-  GPIO.output(10,False)
-def esquerda():
-  GPIO.output(7,True)
-  GPIO.output(8,False)
-  GPIO.output(9,False)
-  GPIO.output(10,True)
-
 pinoTrigger = 17
 pinoEcho = 18
 
@@ -55,21 +28,3 @@ def sensor():
   tempocorrido = tempoF - tempoI
   distancia = (tempocorrido)/2 * 34326 
   return distancia
-
-  print("Distancia: ",distancia)
-  time.sleep(0.5)
-lista = [esquerda, direita]
-
-try:
-  while True:
-
-    if(distancia()<10):
-      parar()
-      tras()
-      time.sleep(0.3)
-      random.choice(lista)()
-      time.sleep(0.3)
-    else:
-      frente()
-
-  parar()
