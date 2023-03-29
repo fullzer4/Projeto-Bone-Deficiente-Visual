3b2fd44eb3cc138364fec0de6ba72e48604b78ca
Gabriel Pelizzaro Pereira <53914594+fullzer4@users.noreply.github.com>
2023-03-28 23:26:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
+#def sensor()
+
 import RPi.GPIO as GPIO
 import time
-import os
-import random
-os.system('cls' if os.name == 'nt' else 'clear')
-
 GPIO.setmode(GPIO.BCM)
-pinoTrigger = 17
-pinoEcho = 18
+GPIO.setwarnings(False)
 
-GPIO.setup(pinoTrigger, GPIO.OUT)
-GPIO.setup(pinoEcho, GPIO.IN)
+global pinoTrigger = 17
+global pinoEcho = 18
+global GPIO.setup(pinoTrigger, GPIO.OUT)
+global GPIO.setup(pinoEcho, GPIO.IN)
 
 def sensor():
   try:
@@ -17,14 +16,23 @@ def sensor():
       GPIO.output(pinoTrigger,False)
       time.sleep(1)
       GPIO.output(pinoTrigger,True)
-      time.sleep(0.000001)
+      time.sleep(0.0001)
       GPIO.output(pinoTrigger,False)
       tempoI = time.time()
       while GPIO.input(pinoecho)==0:
         tempoI = time.time()
-      while GPIO.input(pinoecho)==1:
-        tempoF = time.time()
+        while GPIO.input(pinoecho)==1:
+          tempoF = time.time()
     
   tempocorrido = tempoF - tempoI
   distancia = (tempocorrido)/2 * 34326 
-  return distancia
+
+  print("Distancia: ",distancia)
+  return(distancia)
+  time.sleep(0.5)
+
+while True:
+  if sensor() < 80:
+    GPIO.output(buzina,True)
+    time.sleep(30)
+    GPIO.output(buzina,False)
